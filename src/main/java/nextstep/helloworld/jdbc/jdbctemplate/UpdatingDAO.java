88dd31fcc6c4e7ed100fc7a71a86f2433d1dbe37
hyeon9mak <jinha3507@gmail.com>
2021-04-14 14:09:52 +0900

    refactor: fix return value for updating test success

--- src/main/java/nextstep/helloworld/jdbc/jdbctemplate/UpdatingDAO.java
+++ src/main/java/nextstep/helloworld/jdbc/jdbctemplate/UpdatingDAO.java
@@ -1,39 +1,44 @@
 package nextstep.helloworld.jdbc.jdbctemplate;
 
+import java.sql.PreparedStatement;
 import nextstep.helloworld.jdbc.Customer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UpdatingDAO {
-    private JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
+        Customer customer = new Customer(
+            resultSet.getLong("id"),
+            resultSet.getString("first_name"),
+            resultSet.getString("last_name")
+        );
+        return customer;
+    };
+    private final JdbcTemplate jdbcTemplate;
 
     public UpdatingDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-
-    private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
-        Customer customer = new Customer(
-                resultSet.getLong("id"),
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name")
-        );
-        return customer;
-    };
 
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public void insert(Customer customer) {
         String sql = "insert into customers (first_name, last_name) values (?, ?)";
+        jdbcTemplate.update(sql, customer.getFirstName(), customer.getLastName());
     }
+
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public int delete(Long id) {
         String sql = "delete from customers where id = ?";
-        return 0;
+        return jdbcTemplate.update(sql, id);
     }
 
     /**
@@ -41,6 +46,12 @@ public class UpdatingDAO {
      */
     public Long insertWithKeyHolder(Customer customer) {
         final String sql = "insert into customers (first_name, last_name) values (?, ?)";
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        return Long.valueOf(jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, customer.getFirstName());
+            ps.setString(2, customer.getLastName());
+            return ps;
+        }, keyHolder));
     }
 }
